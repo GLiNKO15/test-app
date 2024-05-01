28d5ed72410d440b5b4c9eea43c7d3f822f02773
Artem Baranov <artembaranov656@gmail.com>
2024-05-01 19:21:35 +0800

    test

--- src/app/core/login/login.component.ts
+++ src/app/core/login/login.component.ts
@@ -4,7 +4,12 @@ import {
   Component,
   inject,
 } from '@angular/core';
-import { Validator, FormControl, ReactiveFormsModule } from '@angular/forms';
+import {
+  Validator,
+  FormControl,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { UserApi } from './services/users.service';
 import { UserDTOI } from './services/users-interface';
 import {
@@ -31,14 +36,14 @@ import { CommonModule } from '@angular/common';
   templateUrl: './login.component.html',
 })
 export class LoginComponent {
-  public input = new FormControl('');
+  public readonly input = new FormControl('', Validators.required);
   public isLoading = false;
   public isError = false;
   private timeLeft: number = 60;
   public user$: Observable<unknown> | null = null;
   private timer = timer(1000, 1000);
   public vmTimer: number = this.timeLeft;
-  private usersApi = inject(UserApi);
+  private readonly usersApi = inject(UserApi);
 
   startTimer() {
     this.isLoading = true;
@@ -49,11 +54,12 @@ export class LoginComponent {
   }
 
   public onSubmit() {
-    if(!this.input.valid) return 
-
+    if (!this.input.valid) return;
     this.user$ = this.usersApi.get<UserDTOI[]>().pipe(
       first(),
       map((users) => users.filter((elem) => elem.username == this.input.value)),
+      // тут можно было написать поменьше, но это спицифика api,
+      // могу делать запрос только за всеми users
       tap((a) => (this.isError = true)),
       map((user) => {
         if (user.length == 1) {
